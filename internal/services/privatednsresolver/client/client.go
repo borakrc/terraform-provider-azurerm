f15fc9d4d40e9e08c0dd8962e5fbfde3e75effcf
Bora Kirca <bora.kirca@bidbax.no>
2026-06-16 13:53:49 +0200

    privatednsresolver: register DNS security policy SDK clients (2025-05-01)
    Adds clients for the four new dnsresolver/2025-05-01 SDK packages that
    back the upcoming DNS security policy resources:
    
      - dnsresolverpolicies
      - dnsresolverdomainlists
      - dnssecurityrules
      - dnsresolverpolicyvirtualnetworklinks
    
    The existing six resources continue to use the 2022-07-01 SDK packages
    that already shipped, so this change is purely additive.
    
    Refs #28597

--- internal/services/privatednsresolver/client/client.go
+++ internal/services/privatednsresolver/client/client.go
@@ -12,16 +12,24 @@ import (
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2022-07-01/inboundendpoints"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2022-07-01/outboundendpoints"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2022-07-01/virtualnetworklinks"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2025-05-01/dnsresolverdomainlists"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2025-05-01/dnsresolverpolicies"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2025-05-01/dnsresolverpolicyvirtualnetworklinks"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/dnsresolver/2025-05-01/dnssecurityrules"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/common"
 )
 
 type Client struct {
-	DnsForwardingRulesetsClient *dnsforwardingrulesets.DnsForwardingRulesetsClient
-	DnsResolversClient          *dnsresolvers.DnsResolversClient
-	ForwardingRulesClient       *forwardingrules.ForwardingRulesClient
-	InboundEndpointsClient      *inboundendpoints.InboundEndpointsClient
-	OutboundEndpointsClient     *outboundendpoints.OutboundEndpointsClient
-	VirtualNetworkLinksClient   *virtualnetworklinks.VirtualNetworkLinksClient
+	DnsForwardingRulesetsClient                *dnsforwardingrulesets.DnsForwardingRulesetsClient
+	DnsResolverDomainListsClient               *dnsresolverdomainlists.DnsResolverDomainListsClient
+	DnsResolverPoliciesClient                  *dnsresolverpolicies.DnsResolverPoliciesClient
+	DnsResolverPolicyVirtualNetworkLinksClient *dnsresolverpolicyvirtualnetworklinks.DnsResolverPolicyVirtualNetworkLinksClient
+	DnsResolversClient                         *dnsresolvers.DnsResolversClient
+	DnsSecurityRulesClient                     *dnssecurityrules.DnsSecurityRulesClient
+	ForwardingRulesClient                      *forwardingrules.ForwardingRulesClient
+	InboundEndpointsClient                     *inboundendpoints.InboundEndpointsClient
+	OutboundEndpointsClient                    *outboundendpoints.OutboundEndpointsClient
+	VirtualNetworkLinksClient                  *virtualnetworklinks.VirtualNetworkLinksClient
 }
 
 func NewClient(o *common.ClientOptions) (*Client, error) {
@@ -31,11 +39,35 @@ func NewClient(o *common.ClientOptions) (*Client, error) {
 		return nil, fmt.Errorf("building DnsForwardingRulesetsClient client: %+v", err)
 	}
 
+	dnsResolverDomainListsClient, err := dnsresolverdomainlists.NewDnsResolverDomainListsClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building DnsResolverDomainListsClient client: %+v", err)
+	}
+	o.Configure(dnsResolverDomainListsClient.Client, o.Authorizers.ResourceManager)
+
+	dnsResolverPoliciesClient, err := dnsresolverpolicies.NewDnsResolverPoliciesClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building DnsResolverPoliciesClient client: %+v", err)
+	}
+	o.Configure(dnsResolverPoliciesClient.Client, o.Authorizers.ResourceManager)
+
+	dnsResolverPolicyVirtualNetworkLinksClient, err := dnsresolverpolicyvirtualnetworklinks.NewDnsResolverPolicyVirtualNetworkLinksClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building DnsResolverPolicyVirtualNetworkLinksClient client: %+v", err)
+	}
+	o.Configure(dnsResolverPolicyVirtualNetworkLinksClient.Client, o.Authorizers.ResourceManager)
+
 	dnsResolversClient, err := dnsresolvers.NewDnsResolversClientWithBaseURI(o.Environment.ResourceManager)
 	o.Configure(dnsResolversClient.Client, o.Authorizers.ResourceManager)
 	if err != nil {
 		return nil, fmt.Errorf("building DnsResolversClient client: %+v", err)
 	}
+
+	dnsSecurityRulesClient, err := dnssecurityrules.NewDnsSecurityRulesClientWithBaseURI(o.Environment.ResourceManager)
+	if err != nil {
+		return nil, fmt.Errorf("building DnsSecurityRulesClient client: %+v", err)
+	}
+	o.Configure(dnsSecurityRulesClient.Client, o.Authorizers.ResourceManager)
 
 	forwardingRulesClient, err := forwardingrules.NewForwardingRulesClientWithBaseURI(o.Environment.ResourceManager)
 	o.Configure(forwardingRulesClient.Client, o.Authorizers.ResourceManager)
@@ -62,11 +94,15 @@ func NewClient(o *common.ClientOptions) (*Client, error) {
 	}
 
 	return &Client{
-		DnsForwardingRulesetsClient: dnsForwardingRulesetsClient,
-		DnsResolversClient:          dnsResolversClient,
-		ForwardingRulesClient:       forwardingRulesClient,
-		InboundEndpointsClient:      inboundEndpointsClient,
-		OutboundEndpointsClient:     outboundEndpointsClient,
-		VirtualNetworkLinksClient:   virtualNetworkLinksClient,
+		DnsForwardingRulesetsClient:                dnsForwardingRulesetsClient,
+		DnsResolverDomainListsClient:               dnsResolverDomainListsClient,
+		DnsResolverPoliciesClient:                  dnsResolverPoliciesClient,
+		DnsResolverPolicyVirtualNetworkLinksClient: dnsResolverPolicyVirtualNetworkLinksClient,
+		DnsResolversClient:                         dnsResolversClient,
+		DnsSecurityRulesClient:                     dnsSecurityRulesClient,
+		ForwardingRulesClient:                      forwardingRulesClient,
+		InboundEndpointsClient:                     inboundEndpointsClient,
+		OutboundEndpointsClient:                    outboundEndpointsClient,
+		VirtualNetworkLinksClient:                  virtualNetworkLinksClient,
 	}, nil
 }
